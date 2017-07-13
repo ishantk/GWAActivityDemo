b3daee8bebb514381f6c92618580406246665f5e
Ishant Kumar <er.ishant@gmail.com>
2017-07-13 12:10:12 +0530

    Fragments

---
+++ app/src/main/java/com/auribises/gwaactivitydemo/FragmentsDemoActivity.java
@@ -0,0 +1,30 @@
+package com.auribises.gwaactivitydemo;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentsDemoActivity extends AppCompatActivity {
+
+    UpperFragment upperFragment;
+    LowerFragment lowerFragment;
+
+    FragmentManager fragmentManager;
+
+    void initViews(){
+        upperFragment = new UpperFragment();
+        lowerFragment = new LowerFragment();
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.upperFrame,upperFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.lowerFrame,lowerFragment).commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragments_demo);
+
+        initViews();
+    }
+}
